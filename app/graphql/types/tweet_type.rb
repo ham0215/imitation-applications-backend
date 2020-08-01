488933af4990457b1e43643bd752b183b00bac7b
naoto hamada <hamada@vis-its.com>
2020-08-02 01:00:18 +0900

    Add tweet (#5)

---
+++ app/graphql/types/tweet_type.rb
@@ -0,0 +1,13 @@
+module Types
+  class TweetType < BaseObject
+    field :tweetId, ID, null: false
+    field :name, String, null: false
+    field :userId, String, null: false
+    field :tweetTime, String, null: false
+    field :avatarImg, String, null: false
+    field :avatarAlt, String, null: false
+    field :text, String, null: false
+    field :img, String, null: false
+    field :imgAlt, String, null: false
+  end
+end
