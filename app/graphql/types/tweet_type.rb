641393e25ad8ffa236dfddcb7988be50ccc0c939
naoto hamada <hamada@vis-its.com>
2020-08-02 00:49:44 +0900

    Fix tweet type

--- app/graphql/types/tweet_type.rb
+++ app/graphql/types/tweet_type.rb
@@ -1,9 +1,13 @@
 module Types
   class TweetType < BaseObject
     field :id, ID, null: false
-    field :user, Types::UserType, null: false
+    field :name, String, null: false
+    field :user_id, String, null: false
+    field :avatar_image, String, null: false
+    field :avatar_alt, String, null: false
     field :created_at,  GraphQL::Types::ISO8601DateTime, null: false
     field :text, String, null: false
-    field :tweet_images, Types::TweetImageType.connection_type, null: false
+    field :tweet_image, String, null: false
+    field :tweet_alt, String, null: false
   end
 end
