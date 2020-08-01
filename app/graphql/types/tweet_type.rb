4cd864ceebd94458838be1893583d1afea7c0cac
naoto hamada <hamada@vis-its.com>
2020-08-02 00:56:23 +0900

    Fix rubocop

--- app/graphql/types/tweet_type.rb
+++ app/graphql/types/tweet_type.rb
@@ -3,7 +3,7 @@ module Types
     field :tweetId, ID, null: false
     field :name, String, null: false
     field :userId, String, null: false
-    field :tweetTime,  String, null: false
+    field :tweetTime, String, null: false
     field :avatarImg, String, null: false
     field :avatarAlt, String, null: false
     field :text, String, null: false
