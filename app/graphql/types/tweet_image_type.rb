aa8073e8b29afe1a6ce0cc299f8e3680a7cf1ce5
naoto hamada <hamada@vis-its.com>
2020-08-02 00:43:46 +0900

    Remove unused classes

--- app/graphql/types/tweet_image_type.rb
+++
@@ -1,7 +0,0 @@
-module Types
-  class TweetImageType < BaseObject
-    field :id, ID, null: false
-    field :url, String, null: false
-    field :alt, String, null: false
-  end
-end
--- app/graphql/types/user_type.rb
+++
@@ -1,9 +0,0 @@
-module Types
-  class UserType < BaseObject
-    field :id, ID, null: false
-    field :name, String, null: false
-    field :key, String, null: false
-    field :avatar_image, String, null: false
-    field :avatar_alt, String, null: false
-  end
-end
